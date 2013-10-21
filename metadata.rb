05615e7c0f4123e5b7a3330cfea6af0464996d96
Dave Shawley <daveshawley@gmail.com>
2013-10-20 21:26:41 -0400

    metadata.rb: Add recipe statement.

--- metadata.rb
+++ metadata.rb
@@ -7,3 +7,8 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
 depends 'python'
+['ubuntu', 'centos'].each do |platform|
+  supports platform
+end
+
+recipe 'devpi::server', 'Installs the devpi-server package.'
