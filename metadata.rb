6448f6dd883c1b02bc9e8237666a19e6e8cf3213
Dave Shawley <daveshawley@gmail.com>
2013-12-20 07:39:40 -0500

    metadata: Bump version to 1.1.0.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'daveshawley@gmail.com'
 license          'Apache 2.0'
 description      'Installs/Configures a devpi server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.1'
+version          '1.1.0'
 
 depends 'nginx'
 depends 'python'
